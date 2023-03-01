e214ce8e00fed9833d938ccf124b0de97edd7ad0
Rishad Alam <rishad.s.a@gmail.com>
2023-02-28 23:13:05 -0500

    function in js and html

---
+++ script.js
@@ -0,0 +1,10 @@
+
+/* eslint-disable no-console */
+var drawBarChart = function (data, options, element) {};
+var data = [1, 2, 3, 4, 5];
+var options = { width: 100, height: 100 };
+var element = "#barchart";
+console.log(drawBarChart(data, options, element));
+
+
+
