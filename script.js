65a16a6c33d033dfde18b3ec55c30e6c8ff9eead
Rishad Alam <rishad.s.a@gmail.com>
2023-03-01 15:21:36 -0500

    html and css setup for barchart, js structure

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 
 /* eslint-disable no-console */
-var drawBarChart = function (data, options, element) {};
+var drawBarChart = function (data, options, element) { console.log(""); };
+
+
 var data = [1, 2, 3, 4, 5];
+var labels = ["a", "b", "c", "d", "e"];
 var options = { width: 100, height: 100 };
 var element = "#barchart";
-console.log(drawBarChart(data, options, element));
+var columns = {};
+var maxValue = data.max;
+for (var i = 0; i < data.length; i++) {
+  (data[i]) = { color: "red", height: ((data[i]) / maxValue) * 100, label: labels[i] };
 
+}
+console.log(drawBarChart(data, options, element));
+console.log(maxValue);
+console.log(data);
 
 //data Input functions//
 
